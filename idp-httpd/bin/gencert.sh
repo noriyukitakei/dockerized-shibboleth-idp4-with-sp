2df89064b5f8b4af187bebc598dd02523c8b46b6
Noriyuki TAKEI <ntakei@sios.com>
2020-03-29 02:11:08 +0900

    certsディレクトリに証明書を入れると、そちらが優先して使われるように修正

--- idp-httpd/bin/gencert.sh
+++ idp-httpd/bin/gencert.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# ApacheのSSL証明書を生成する。
-cd /tmp
-
-openssl genrsa 2048 > server.key
-
-openssl req -new -key server.key <<EOF > server.csr
+if [ -e /tmp/certs/server.crt -a -e /tmp/certs/server.key ]; then
+  # certsディレクトリに証明書が置いてあったらそちらを優先して配置する。
+  mv /tmp/certs/server.crt /usr/local/apache2/conf/server.crt
+  mv /tmp/certs/server.key /usr/local/apache2/conf/server.key
+else
+  # ApacheのSSL証明書を生成する。
+  cd /tmp
+  
+  openssl genrsa 2048 > server.key
+  
+  openssl req -new -key server.key <<EOF > server.csr
 JP
 Tokyo
 Minato-ku
@@ -16,9 +21,12 @@ idp.example.org
 
 
 EOF
+  
+  openssl x509 -days 3650 -req -signkey server.key < server.csr > server.crt
+  
+  mv /tmp/server.key /usr/local/apache2/conf/server.key
+  mv /tmp/server.crt /usr/local/apache2/conf/server.crt
+  rm /tmp/server.csr
+fi
 
-openssl x509 -days 3650 -req -signkey server.key < server.csr > server.crt
 
-mv /tmp/server.key /usr/local/apache2/conf/server.key
-mv /tmp/server.crt /usr/local/apache2/conf/server.crt
-rm /tmp/server.csr
--- sp/bin/init-sp.sh
+++ sp/bin/init-sp.sh
@@ -5,3 +5,9 @@ cd /etc/shibboleth
 for file in `\find /tmp/patch -maxdepth 1 -type f`; do
     patch -b -p0 < $file
 done
+
+if [ -e /tmp/certs/server.crt -a -e /tmp/certs/server.key ]; then
+  # certsディレクトリに証明書が置いてあったらそちらを優先して配置する。
+  mv -f /tmp/certs/server.crt /etc/pki/tls/certs/localhost.crt
+  mv -f /tmp/certs/server.key /etc/pki/tls/private/localhost.key
+fi
