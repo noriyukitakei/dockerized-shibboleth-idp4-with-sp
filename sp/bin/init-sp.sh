fef3f02b7a21674ce94b92851de76e080a0aca3b
Noriyuki TAKEI <ntakei@sios.com>
2020-12-04 11:02:00 +0900

    SPのApacheのHTTPSのポートを10443に変更

--- sp/bin/init-sp.sh
+++ sp/bin/init-sp.sh
@@ -11,3 +11,7 @@ if [ -e /tmp/certs/server.crt -a -e /tmp/certs/server.key ]; then
   mv -f /tmp/certs/server.crt /etc/pki/tls/certs/localhost.crt
   mv -f /tmp/certs/server.key /etc/pki/tls/private/localhost.key
 fi
+
+# httpsの待受ポートを10443に変更する。
+sed -i -e 's/Listen 443 https/Listen 10443 https/g' /etc/httpd/conf.d/ssl.conf 
+sed -i -e 's/<VirtualHost _default_:443>/<VirtualHost _default_:10443>/g' /etc/httpd/conf.d/ssl.conf
